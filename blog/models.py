6f2cf7f10a3913e2a83a8a06f0789ecc23823003
Artur <artur.khobotkov@nure.ua>
2022-03-15 17:35:46 +0200

    Crate category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель(табличка) картегорий"""
+
+    name = models.CharField(verbose_name="Имя", max_length=100)
+    slug = models.SlugField(verbose_name="url", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
